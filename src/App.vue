c659b4c53f942175e5cf67d0d480d931904a08e1
ChristianMinnie <christian.minnie@gmail.com>
2021-10-28 11:19:10 -0500

    yeah

--- src/App.vue
+++ src/App.vue
@@ -1,122 +1,78 @@
 <template>
   <div id="app">
     <!-- ======= Header ======= -->
-    <header id="header" class="d-flex flex-column justify-content-center">
-      <nav id="navbar" class="navbar nav-menu">
-        <ul>
-          <li>
-            <a href="#hero" class="nav-link scrollto active">
-              <i class="bx bx-home"></i>
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="#login" class="nav-link scrollto">
-              <i class="bx bx-book-content"></i>
-              <span>Login</span>
-            </a>
-          </li>
-          <!-- <li>
-            <a href="#about" class="nav-link scrollto">
-              <i class="bx bx-user"></i>
-              <span>About</span>
-            </a>
-          </li> -->
-          <!-- <li>
-            <a href="#resume" class="nav-link scrollto">
-              <i class="bx bx-file-blank"></i>
-              <span>Resume</span>
-            </a>
-          </li> -->
-          <li>
-            <a href="#profile" class="nav-link scrollto">
-              <i class="bx bx-book-content"></i>
-              <span>Profile</span>
-            </a>
-          </li>
-          <!-- <li>
-            <a href="#services" class="nav-link scrollto">
-              <i class="bx bx-server"></i>
-              <span>Services</span>
-            </a>
-          </li> -->
-          <!-- <li>
-            <a href="#contact" class="nav-link scrollto">
-              <i class="bx bx-envelope"></i>
-              <span>Contact</span>
-            </a>
-          </li> -->
-        </ul>
-      </nav>
-      <!-- .nav-menu -->
+    <header id="header" class="fixed-top">
+      <div class="container-fluid d-flex justify-content-between align-items-center">
+        <h1 class="logo me-auto me-lg-0">
+          <a href="/profile">{{ student.first_name }}</a>
+        </h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <!-- <nav id="navbar" class="navbar order-last order-lg-0">
+          <ul>
+            <li><a class="active" href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="resume.html">Resume</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="portfolio.html">Portfolio</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+
+        <div class="header-social-links">
+          <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div> -->
+        <a href="/logout">Logout</a>
+      </div>
     </header>
     <!-- End Header -->
-
-    <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex flex-column justify-content-center">
-      <div class="container" data-aos="zoom-in" data-aos-delay="100">
-        <div>
-          <h1>{{ student }}</h1>
-        </div>
-        <!-- <p>
-          <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span>
-        </p> -->
-        <div class="social-links">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        </div>
-      </div>
-    </section>
-    <!-- End Hero -->
+    <!-- ROUTER VIEW -->
     <router-view />
-
     <!-- ======= Footer ======= -->
     <footer id="footer">
       <div class="container">
-        <h3>{{ student }}</h3>
-        <p>
-          Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.
-        </p>
-        <div class="social-links">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        </div>
         <div class="copyright">
           &copy; Copyright
-          <strong><span>MyResume</span></strong>
+          <strong><span>Kelly</span></strong>
           . All Rights Reserved
         </div>
         <div class="credits">
           <!-- All the links in the footer should remain intact. -->
           <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: [license-url] -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/ -->
           Designed by
           <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
       </div>
     </footer>
-    <!-- End Footer -->
-
-    <!-- <div id="preloader"></div> -->
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
-      <i class="bi bi-arrow-up-short"></i>
-    </a>
+    <!-- End  Footer -->
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      current_student: "Percy",
+      student: {},
     };
+  },
+  created: function () {
+    let student_id = localStorage.getItem("student_id");
+    axios.get(`https://afternoon-reaches-14167.herokuapp.com/students/${student_id}`).then((response) => {
+      console.log("Student Show", response);
+      this.student = response.data;
+      this.experiences = response.data.experiences;
+      this.educations = response.data.educations;
+      this.projects = response.data.projects;
+      this.skills = response.data.skills;
+    });
   },
 };
 </script>
